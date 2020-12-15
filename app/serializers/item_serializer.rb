bd95f666ffbf9c0d1a35518280eefb128300064c
Jose Lopez <joselopez11394@gmail.com>
2020-12-14 19:20:19 -0500

    refactors the serializers and changes the controllers to work with the new serializers

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,33 +1,4 @@
 class ItemSerializer
-  def self.format_items(items)
-    all_data = {data: []}
-    items.each do |item|
-      all_data[:data] << {
-        id: item.id,
-        type: "item",
-        attributes: {
-          name: item.name,
-          description: item.description,
-          unit_price: item.unit_price,
-          merchant_id: item.merchant_id
-        }
-      }
-    end
-    all_data
-  end
-
-  def self.item_details(item)
-    {
-      data: {
-        id: item.id,
-        type: "item",
-        attributes: {
-          name: item.name,
-          description: item.description,
-          unit_price: item.unit_price,
-          merchant_id: item.merchant_id
-        }
-      }
-    }
-  end
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :description, :unit_price, :merchant_id
 end
